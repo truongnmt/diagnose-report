38b6eb33f39840fb6b987a2064248f2976df2ade
TruongNM <truongkechun@gmail.com>
2018-03-16 10:38:17 +0700

    first commit

---
+++ app/src/main/java/net/simplifiedlearning/simplifiedcoding/URLs.java
@@ -0,0 +1,14 @@
+package net.simplifiedlearning.simplifiedcoding;
+
+/**
+ * Created by Belal on 9/5/2017.
+ */
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://10.0.2.2:8888/login-register/api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
